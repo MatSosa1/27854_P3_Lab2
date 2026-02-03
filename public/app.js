afcd3e70a38bbe2ddd20c0b03d31906554581919
MatSosa1 <dmsosa3@espe.edu.ec>
2026-02-03 10:45:54 -0500

    Lint Fixes

--- public/app.js
+++ public/app.js
@@ -5,4 +5,4 @@ function sum(a, b) {
 const num1 = 5;
 const num2 = 8;
 
-document.getElementById('out').textContent = `${num1} + ${num2} = ${sum(num1, num2)}`
+document.getElementById('out').textContent = `${num1} + ${num2} = ${sum(num1, num2)}`;
